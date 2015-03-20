f282e9c8c3a269150507734bfe23e0c17c6fe548
mtakebayashi16 <mtakebayashi16@punahou.edu>
2015-03-20 12:50:24 -1000

    Third Commit
    1.5 hours - Added more stuff to the display screen (displays card suit
    and value) and tried messing around with array lists (did not work so
    decided to stick with an array).
    
    Card hand does print, but card values aren’t randomized (prints the
    default values)

--- Deck.java
+++ Deck.java
@@ -2,9 +2,9 @@ public class Deck{
   private Cards[] deck = new Cards[52];
   private int usedCards;    //keeps track of how many cards have been dealt
   
+  
   public Deck(){
     int counter = 0; //keeps track of created cards (should be 52)
-    
     for (int suit = 0; suit <= 3; suit++) {
       for (int value = 1; value <= 13; value++) {
         deck[counter] = new Cards(value,suit);
